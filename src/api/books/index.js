928479012c7531ea41ca53f0132bb89a6319b8df
Hwarang Choi <az8606@naver.com>
2019-04-10 21:20:30 +0900

    connect with mongodb

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -1,6 +1,8 @@
 import Router from 'koa-router';
 
 const books = new Router();
+
+// export const 를 이용해서 변수명을 통해 알아서 매칭
 import {list, create, deleted, replace, update} from './books.controller';
 
 books.get('/', list);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import Koa from 'koa';
 import Router from 'koa-router';
 
@@ -5,10 +8,28 @@ const app = new Koa();
 const router = new Router();
 import api from './api';
 
-router.use('/api', api.routes());
+import mongoose from 'mongoose';
+import bodyParser from 'koa-bodyparser';
 
+// node에 자체적으로 있는 Promise를 사용하도록 설정
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser:true
+}).then(
+    (response) => {
+        console.log('Successfully connected to mongodb');
+    }
+).catch(e => {
+    console.error(e);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser());
+
+router.use('/api', api.routes());
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-    console.log('heurm server is listening to port 4000');
+app.listen(PORT, () => {
+    console.log(`herum server is listening to port ${PORT}`);
 });
